1523ea6e95dc1bc0b781e95e3e53307161e44b8a
Rod <rodrigue@upowa.org>
2023-11-04 06:28:02 +0100

    Create, List and Detail User

--- server.js
+++ server.js
@@ -1,19 +1,50 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const User = require("./models/user");
 const app = express();
+
+app.use(express.json());
 
 //End-points
 app.get("/", (req, res) => {
   res.send("Hello Node addict");
 });
 
-app.get("/user", (req, res) => {
-  res.send("Hello and welcome User");
+app.get("/user/all", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
 });
 
+app.get("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/user/add", async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+mongoose.set("strictPopulate", false);
 mongoose
   .connect(
-    "mongodb+srv://rodrigue:Somtofioko237@cluster0.u1g7hb1.mongodb.net/express-server?retryWrites=true&w=majority"
+    "mongodb+srv://rodrigue:somtofiOko27@cluster0.u1g7hb1.mongodb.net/Express-Server?retryWrites=true&w=majority"
   )
   .then(() => {
     console.log("Connected to MongoDB");
