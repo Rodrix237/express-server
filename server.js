eee9bb66c9ddb380dddcfde6bd64d2a0072ae07c
Rod <rodrigue@upowa.org>
2023-11-04 04:13:45 +0100

    Connect to Atlas MongoDB

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-console.log("Server is running");
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+//End-points
+app.get("/", (req, res) => {
+  res.send("Hello Node addict");
+});
+
+app.get("/user", (req, res) => {
+  res.send("Hello and welcome User");
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://rodrigue:Somtofioko237@cluster0.u1g7hb1.mongodb.net/express-server?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log(Error);
+  });
